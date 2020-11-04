2079f32173fdbcee28d627a64548384065b2ef28
KrisClem <71985892+KrisClem@users.noreply.github.com>
2020-11-04 12:52:05 -0800

    Fixed space between buttons

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 $('#js-shopping-list-form').submit(function(e){
     e.preventDefault();
     const entry = $('#shopping-list-entry').val();
-    $('.shopping-list').append('<li><span class="shopping-item">' + entry + 
-    '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>')
+    $('.shopping-list').append(
+    `<li>
+        <span class="shopping-item">` + entry + `</span>
+        <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+        </div>
+    </li>`)
 });
 
 function itemChecked() {
